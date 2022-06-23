516e71c7ee0ec779fc518b4a26ae87fef7ad9a43
MatheusAndradeMelo <mandrademelo77@gmail.com>
2022-06-23 00:40:16 -0300

    create project, add components like header, resume, resumeItem, install styled-components, and react-icons, put a global css

---
+++ src/components/Header/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    height: 150px;
+    text-align: center;
+    background: teal;
+`;
+
+export const Header = styled.h1`
+    height: 150px;
+    text-align: center;
+    background: teal;
+`;
+
+export const Title = styled.div`
+    padding-top: 20px;
+    color: #fff;
+`;
